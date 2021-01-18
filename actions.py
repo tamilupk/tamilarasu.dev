8dad9692467d404f1e67c1b5158d6a60e8fed972
Tamilarasu Ulaganathan <tamilarasu.ku@gmail.com>
2021-01-18 00:48:49 -0500

    Actions test 7

--- actions.py
+++
@@ -1,5 +0,0 @@
-import os
-
-print(f'AWS_DEFAULT_REGION: {os.getenv("AWS_DEFAULT_REGION")}')
-print(f'AWS_ACCESS_KEY_ID: {os.getenv("AWS_ACCESS_KEY_ID")}')
-print(f'AWS_SECRET_ACCESS_KEY: {os.getenv("AWS_SECRET_ACCESS_KEY")}')
